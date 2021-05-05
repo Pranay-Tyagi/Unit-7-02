d5eb38c495d05487fa3be8a41dc78f252305e611
PranayTyagi1 <>
2021-05-05 15:08:47 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,11 @@
+document.querySelector('button').addEventListener('click', function () {
+  let number = document.querySelector('input').value
+  number = parseInt(number) 
+  if (number < 0) {
+  alert('negative number')
+} else if (number === 0) {
+  alert('neither')
+} else {
+  alert('positve number')
+}
+})
