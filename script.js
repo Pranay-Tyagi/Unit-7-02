def5946a93ae1acf7402b1e55d59ae7a53095bba
PranayTyagi1 <>
2021-05-05 15:20:24 +0000

    indentation

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-document.querySelector('button').addEventListener('click', function () {
+document.querySelector('button').addEventListener('click', function() {
   let number = document.querySelector('input').value
-  number = parseInt(number) 
+  number = parseInt(number)
   if (number < 0) {
-  alert('negative number')
-} else if (number === 0) {
-  alert('neither')
-} else {
-  alert('positve number')
-}
+    alert('negative number')
+  } else if (number === 0) {
+    alert('neither')
+  } else {
+    alert('positve number')
+  }
 })
